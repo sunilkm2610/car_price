01170318cf5cd7909d1277c537c41dd05ec8a460
sunilkm2610 <sunilkm2610@gmail.com>
2021-02-05 16:14:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -22,10 +22,9 @@ def price_predication(km_driven,fuel,transmission,owner,old_year):
     return predication
 def selection(fuel,transmission,owner) :
     l = []
-    f =  {'Petrol':4 ,'Diesel':1 ,'CNG' :0,'LPG':3, 'Electric':2}
-    t =  { 'Manual':1 ,'Automatic':0}
-    o = {'First Owner':0 ,'Second Owner':2 ,'Fourth & Above Owner':1 ,'Third Owner':4,
- 'Test Drive Car':3}
+    f = {'Petrol':4 ,'Diesel':1 ,'CNG' :0,'LPG':3, 'Electric':2}
+    t = { 'Manual':1 ,'Automatic':0}
+    o = {'First Owner':0 ,'Second Owner':2 ,'Fourth & Above Owner':1 ,'Third Owner':4,'Test Drive Car':3}
     
 
     l.append(f[fuel])
@@ -36,18 +35,21 @@ def selection(fuel,transmission,owner) :
         
 def main():
     
+    
     page_bg_img = '''
     <style>
-    body {
-            background-image: url("https://images.wallpaperscraft.com/image/rolls_royce_wraith_movement_side_view_111199_1920x1080.jpg");
+    body {  
+            
+            background-image: url('https://images.unsplash.com/photo-1519641471654-76ce0107ad1b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1651&q=80');
             background-size: cover;
             
             }
     </style>
     
     '''
-    
+    #st.selectbox('Car Company',('HYUNDAI','HONDA','SUZUKI'))
     st.markdown(page_bg_img, unsafe_allow_html=True)
+    
     
     Km_driven = st.number_input('Km_driven',1000,step=1000)
     
@@ -56,14 +58,19 @@ def main():
     print()
     transmission = st.sidebar.selectbox('Transmission',('Manual' ,'Automatic'))
     owner = st.sidebar.selectbox('Owner',('First Owner' ,'Second Owner' ,'Fourth & Above Owner' ,'Third Owner',
- 'Test Drive Car'))
+    'Test Drive Car'))
     oldyear = st.sidebar.slider('Old Year',0,30)
+    st.sidebar.write(oldyear)
     k= [Km_driven]+ selection(fuel,transmission,owner)+[oldyear]
     x = "{:,}".format((round((price_predication(k[0],k[1],k[2],k[3],k[4]))[0])))
     
-    if st.button('Calculate'):
-        print(st.sidebar.header('Your Car Price is'+ ' '+ x))
+    if st.sidebar.button('Calculate'):
+        
+        st.sidebar.header('Car Price in 2021'+ ' ::: '+'₹' +str(x))
     
+    if st.button('About us'):
+        
+        st.sidebar.write('This project based on previous car dataset and predict prices with help of Random Forest Regressor')
     
     
     
